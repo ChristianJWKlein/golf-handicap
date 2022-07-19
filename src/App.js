163585832edc87b48b76a29af17313d40931a975
Christian JW Klein <cklein010715@gmail.com>
2022-07-19 00:30:34 -0400

    Framework initital set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css'
+import Questionnaire from './components/Questionnaire'
 
 function App() {
   return (
     <>
       <h1>Golf Handicap</h1>
+      <Questionnaire />
     </>
   )
 }
---
+++ src/components/FormQuestions/Nickname.js
@@ -0,0 +1,7 @@
+export default function Nickname() {
+  return (
+    <>
+      <p>hgjhfgjfg</p>
+    </>
+  )
+}
